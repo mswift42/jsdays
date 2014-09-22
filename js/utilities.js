5bbde4d87d53f1f9988c76a911b6248aceb17ee3
martin.haesler <martin.haesler@gmail.com>
2014-09-22 21:12:19 +0100

    Add navbarSetActive fn

--- js/utilities.js
+++ js/utilities.js
@@ -1,10 +1,25 @@
+/*global document */
 "use strict";
 var U = {};
 U.$ = function(id) {
     return document.getElementById(id);
 
+
+
 };
 
 U.$cllist = function(cls) {
     return document.getElementsByClassName(cls);
+    
+};
+
+U.navbarSetActive = function(activeelement) {
+    var elements = document.getElementById('navlinks').children;
+    for (var i = 0;i<elements.length;i++) {
+        if (activeelement === elements[i].id) {
+        elements[i].className = "active";
+        } else {
+            elements[i].className = "";
+        }
+    }
 };
