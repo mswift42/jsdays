7676cfd2205647501024e0b76d34da54e286bb0b
martin.haesler <martin.haesler@gmail.com>
2014-10-06 17:39:32 +0100

    Add muteTask fn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* global document , $ */
+/* global document , $, U*/
 'use strict';
 
 function activeLink(elem) {
@@ -12,11 +12,28 @@ function setActive() {
     U.navbarSetActive(activeLink(loc[loc.length-1]));
 }
 $(document).ready(function() {
+    setActive();
     $('.datepicker').datepicker({
         autoclose:true,
-        format:"DD-M-yyyy",
+        format:"DD, M yyyy",
         todayHighlight:true});
 });
 
+function readDate(datestring) {
+    return new Date(datestring);
+}
+
+function findStatusDone(elem) {
+    return elem.innerHTML.match('DONE');
+}
+
+function muteTask() {
+    var elems = document.getElementsByClassName('singletask');
+    for (var i = 0; i< elems.length; i++) {
+        if (findStatusDone(elems[i].children[0])) {
+            elems[i].className=('singletask-done');
+        }
+    }
+}
 
 window.onload = setActive;
