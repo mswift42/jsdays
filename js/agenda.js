1591b503ecf822df7fea02f4583894c21be72990
martin.haesler <martin.haesler@gmail.com>
2014-10-08 18:35:13 +0100

    Add buildAgenda and test

--- js/agenda.js
+++ js/agenda.js
@@ -22,6 +22,18 @@ function readDate(datestring) {
 // addDays - add n days to a given
 // date object.
 function addDays(day, n) {
+    'use strict';
     var nday = new Date();
     return nday.setDate(day.getDate() + n);
+}
+// buildAgenda - return an array of 10 days
+// from startday - 3 days to + 7 days.
+function buildAgenda(startday) {
+    'use strict';
+    var dates = [];
+    for (var i = -3 ; i<8 ; i++) {
+        var day = new Date(addDays(startday,i));
+        dates.push(day);
+    }
+    return dates;
 }
--- tests.js
+++ tests.js
@@ -22,9 +22,26 @@ QUnit.test("test addDays", function(assert) {
     var day2 = new Date(addDays(day1,1));
     var day3 = new Date(addDays(day1,2));
     var day4 = new Date(addDays(day2,2));
+    var day5 = new Date(addDays(day3,-2)); // check if subtraction of days works
+    var day6 = new Date(addDays(day1,-1));
+    var day7 = new Date(addDays(day1,0)); // check if no additions returns same day.
     assert.equal(day2.getMonth(), 9);
     assert.equal(day2.getDate(), 2);
     assert.equal(day3.getDate(), 3);
     assert.equal(day4.getDate(), 4);
     assert.equal(indToWeekDay(day4.getDate()), "Thursday");
+    assert.equal(day5.getDate(), 1);
+    assert.equal(day6.getDate(),30);
+    assert.equal(day6.getMonth(),8);
+    assert.equal(day7.getDate(),1);
+});
+
+QUnit.test("test buildAgenda", function(assert) {
+    var day = new Date(2014,10,1);
+    var ag = buildAgenda(day);
+    assert.equal(ag[0].getDate(),28);
+    assert.equal(ag[0].getMonth(), 8);
+    assert.equal(ag[3].getDate(),1);
+    assert.equal(ag[10].getDate(),8);
+    assert.equal(ag.length,11);
 });
