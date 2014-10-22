6427a2e5b9cec396044e79d68baf977658d9550b
martin.haesler <martin.haesler@gmail.com>
2014-10-22 19:23:59 +0100

    Fix edittask tst

--- jsdays_test.go
+++ jsdays_test.go
@@ -13,23 +13,25 @@ func TestEditTask(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer c.Close()
-	key := datastore.NewKey(c, "Task", "", 1, nil)
+	key := datastore.NewKey(c, "Task", "", 123456789, defaultTaskList(c))
 	id, _ := strconv.ParseInt("123456789", 10, 64)
 	if id != 123456789 {
 		t.Fatal(err)
 	}
-	if _, err := datastore.Put(c, key, &Task{Summary: "some summary", Id: 123456789}); err != nil {
+	if _, err := datastore.Put(c, key, &Task{Summary: "some summary"}); err != nil {
 		t.Fatal(err)
 	}
 	task1ret := Task{}
-	if err := datastore.Get(c, key, &task1ret); err != nil {
+	newkey := keyForID(c, id)
+	if err := datastore.Get(c, newkey, &task1ret); err != nil {
 		t.Fatal(err)
 	}
+	tid := newkey.IntID()
 	if task1ret.Summary != "some summary" {
 		t.Error("Expected <some summary>, got: ", task1ret.Summary)
 	}
-	if task1ret.Id != 123456789 {
-		t.Error("Expected <123456789>, got: ", task1ret.Id)
+	if tid != 123456789 {
+		t.Error("Expected id 123456789, got: ", tid)
 	}
 
 }
